95832f670d1dd485e0324d624cb7acebe4f5371e
SyamKrishnaReddyPulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-02-16 20:29:37 +0530

    Time: 3211 ms (21.08%), Space: 14.5 MB (10.12%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for num in range(len(nums)):
+            for number in range(num+1,len(nums)):
+                if nums[num] + nums[number] == target:
+                    return [num,number]
+                else :
+                    number=number+1            
