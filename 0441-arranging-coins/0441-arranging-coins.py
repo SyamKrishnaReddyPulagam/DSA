986cf51839ff7e452a339034ef25638009f35b7a
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-05-25 23:30:59 +0530

    Time: 21 ms (81.65%), Space: 13.1 MB (98.40%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def arrangeCoins(self, n):
-        if(n==1):
-            return 1
-        x=1
-        while(n>=x):
-            n=n-x
-            x+=1
-        return x-1
+        return int((-1 + sqrt(8 * n + 1)) // 2)
+        
+    
