376a8b39a6ce398be4c5e641b88a0fc7fc3853da
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-05-26 23:21:25 +0530

    Time: 43 ms (60.61%), Space: 14.7 MB (41.53%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        s=list(s)
+        t=list(t)
+        s.sort()
+        t.sort()
+        if(s==t):
+            return True
+        else:
+            return False
