faf9d69fdc8b3d27d098a8507f9420178ecb640b
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-06-01 23:31:39 +0530

    Time: 20 ms (42.94%), Space: 13.3 MB (53.49%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,5 +5,11 @@ class Solution(object):
         elif n==1:
             return x
         else:
-            return x**n
+            if n<0:
+                n=abs(n)
+                x=1/x
+            if n%2==0:
+                return self.myPow(x*x,n/2)
+            else:
+                return x*self.myPow(x*x,n/2)
        
