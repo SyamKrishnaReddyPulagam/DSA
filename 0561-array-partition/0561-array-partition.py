be4f1608278a6f918b572406ab613d1286540c77
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-06-04 17:26:36 +0530

    Time: 242 ms (40.50%), Space: 15.5 MB (31.75%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,7 +1,3 @@
 class Solution(object):
     def arrayPairSum(self, nums):
-        sum=0
-        nums.sort()
-        for i in range(0,len(nums),2):
-            sum+=nums[i]
-        return sum
+        return sum(sorted(nums)[::2])
