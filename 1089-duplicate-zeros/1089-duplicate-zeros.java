d047b08af92939e92e828d5cef2acf3a1c603201
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-06-09 00:20:45 +0530

    Time: 3 ms (56.31%), Space: 44.3 MB (5.94%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        List<Integer> l = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0)
+                l.add(arr[i]);
+            else{
+                l.add(arr[i]);
+                l.add(0);
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=l.get(i);
+        }
+    }
+}
