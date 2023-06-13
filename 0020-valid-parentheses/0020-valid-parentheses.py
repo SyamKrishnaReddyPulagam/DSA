ddd04f74e7dcd2052fc39ec0bbbfcb263e4d37d0
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-06-14 01:04:37 +0530

    Time: 55 ms (5.01%), Space: 13.4 MB (65.28%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isValid(self, s):
+        while len(s) > 0:
+            l = len(s)
+            s = s.replace('()','').replace('{}','').replace('[]','')
+            if l==len(s): return False
+        return True
+        
