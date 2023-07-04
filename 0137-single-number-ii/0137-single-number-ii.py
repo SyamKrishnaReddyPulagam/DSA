d0883cde7e32665803b5cf3342daa608988b48cf
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-07-04 23:00:13 +0530

    Time: 40 ms (83.72%), Space: 14.8 MB (89.66%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,10 +1,16 @@
 class Solution(object):
     def singleNumber(self, nums):
-        a=[]
+        """a=[]
         [a.append(i) for i in nums if i not in a]
         b=[]
         for i in a:
             b.append(nums.count(i))
         print(a)
         c=b.index(1)
-        return a[c]
+        return a[c]"""
+        ones = 0
+        twos = 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
