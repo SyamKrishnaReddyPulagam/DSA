a59fe4e3d1e5efae7b934ee7e43a1d0116adf77e
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-08-25 17:31:25 +0530

    Time: 21 ms (94.69%), Space: 13.2 MB (56.26%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def intToRoman(self, num):
+        a=[[1,'I'],[4,'IV'],[5,'V'],[9,'IX'],[10,'X'],[40,'XL'],[50,'L'],[90,'XC'],[100,'C'],[400,'CD'],[500,'D'],[900,'CM'],[1000,'M']]
+        b=""
+        for i,j in reversed(a):
+            if num//i>0:
+                count=num//i
+                b+=j*count
+                num=num%i
+        return b
