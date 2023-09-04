154b3af3794d5f17bfcf21b4207cbcf8265fb8f7
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-09-04 12:55:10 +0530

    Time: 80 ms (65.74%), Space: 23 MB (11.49%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution(object):
     def mergeKLists(self, lists):
-        def mergelist(self, list1, list2):
+        """def mergelist(self, list1, list2):
             dummy = curr = ListNode(0)   
             while list1 and list2:
                 if list1.val<list2.val:
@@ -30,4 +30,21 @@ class Solution(object):
             l1=mergedlist[0]
             l2=lists[i]
             mergedlist[0]=mergelist(self,l1,l2)
-        return mergedlist[0]
+        return mergedlist[0]"""
+        
+        #using priority queue to solve this
+        h=[]
+        
+        one=two=ListNode(0)
+        
+        for i,x in enumerate(lists):
+            if x:
+                heappush(h,(x.val,i))
+        while h:
+            val,i=heappop(h)
+            two.next=ListNode(val)
+            if lists[i].next:
+                lists[i]=lists[i].next
+                heappush(h,(lists[i].val,i))
+            two=two.next
+        return one.next
