a13f059abbe1b1b18580013987008e195ebaf7bc
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-09-06 11:54:14 +0530

    Time: 151 ms (75.68%), Space: 24.9 MB (20.12%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,7 +1,14 @@
 class Solution(object):
     def rotate(self, nums, k):
+        """
+        #using slicling
         k=k%len(nums)
         nums[:]=nums[-k:]+nums[:-k]
         return nums
-    
-    
+        """
+        x=[0]*len(nums)
+        k=k%len(nums)
+        for i in range(len(nums)):
+            x[(i+k)%len(nums)]=nums[i]
+        nums[:]=x[:]
+        return nums
