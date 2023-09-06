8f039c9fe221270f140a41e7a7fe35e19901fd17
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-09-06 12:01:58 +0530

    Time: 144 ms (91.39%), Space: 24.8 MB (80.52%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,9 +6,20 @@ class Solution(object):
         nums[:]=nums[-k:]+nums[:-k]
         return nums
         """
+        
+        #using an extra array and placing elements with modified indices
         x=[0]*len(nums)
         k=k%len(nums)
         for i in range(len(nums)):
             x[(i+k)%len(nums)]=nums[i]
         nums[:]=x[:]
         return nums
+        
+        """
+        #making inplace change in nums array for each rotation
+        #giving time limit exceed error so above methods are best in case
+        k=k%len(nums)
+        for i in range(k):
+            nums[:]=nums[-1:]+nums[:-1]
+        return nums
+        """
