cd89fbff305c8ef0a6bdce2c71896dba6feacd55
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-09-09 16:52:30 +0530

    Time: 53 ms (23.76%), Space: 16.6 MB (56.07%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -5,7 +5,8 @@ class Solution:
         for n in nums:
             for i in range(len(result)):
                 result.append(result[i]+[n])
-        
+        for i in range(len(result)):
+            result[i]=sorted(result[i])
         ans=[]
         [ans.append(i) for i in result if i not in ans]
         return ans
