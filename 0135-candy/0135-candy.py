92085c69f06fce3eeb1a76ce75e14b3ce7b8710d
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-09-13 15:54:14 +0530

    Time: 150 ms (46.04%), Space: 19.2 MB (91.55%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,11 +1,17 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n=len(ratings)
+        min_can=1*n
         minc=[1]*n
         for i in range(1,n):
             if ratings[i]>ratings[i-1]:
+                min_can-=minc[i]
                 minc[i]=max(minc[i-1]+1,minc[i])
+                min_can+=minc[i]
         for j in range(n-2,-1,-1):
             if ratings[j+1]<ratings[j]:
+                min_can-=minc[j]
                 minc[j]=max(minc[j+1]+1,minc[j])
-        return sum(minc)
+                min_can+=minc[j]
+        return min_can
+        
