c432e3e99595c07bb7deb84893bb50edb0195924
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-09-14 15:34:43 +0530

    Time: 540 ms (91.31%), Space: 29.6 MB (12.39%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        """nums.sort()
         res, l, r = 0, 0 ,len(nums) - 1
 
         while l < r:
@@ -13,4 +13,16 @@ class Solution:
                 res += 1
                 l += 1
                 r -= 1
+        return res
+        """
+        pair = defaultdict(int)
+        res = 0
+        
+        for n in nums:
+            if pair[n]: 
+                res += 1
+                pair[n] -= 1
+            else: 
+                pair[k - n] += 1
+                
         return res
