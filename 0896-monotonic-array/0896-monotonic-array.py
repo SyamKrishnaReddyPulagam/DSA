d6214853acb7ec5f082105000fe6e841e40334dd
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-09-29 08:57:23 +0530

    Time: 824 ms (66.36%), Space: 30.5 MB (83.11%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -1,16 +1,6 @@
-class Solution(object):
-    def isMonotonic(self, nums):
-        a=1
-        b=1
-        for i in range(1,len(nums)):
-            if nums[i-1]<nums[i]:
-                a+=1
-            elif nums[i-1]>nums[i]:
-                b+=1
-            else:
-                a+=1
-                b+=1
-        if a==len(nums) or b==len(nums):
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        x=sorted(nums)
+        if nums== x or nums[::-1]==x:
             return True
         return False
-        
