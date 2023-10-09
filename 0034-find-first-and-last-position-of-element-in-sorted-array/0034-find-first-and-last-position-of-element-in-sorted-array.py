059ae826af604725a67ccf1442526aaf70dee2da
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-10-09 10:25:51 +0530

    Time: 83 ms (63.27%), Space: 17.7 MB (84.76%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,6 +1,31 @@
-class Solution(object):
-    def searchRange(self, nums, target):
-        if target not in nums: 
-            return [-1,-1]
-        else : 
-            return [nums.index(target),nums.index(target) + nums.count(target) - 1]
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def firstele(nums,k):
+            i,j=0,len(nums)-1
+            while i<=j:
+                mid=(i+j)//2
+                if nums[mid]==k:
+                    if mid==0 or nums[mid-1]!=k:
+                        return mid
+                    else:
+                        j=mid-1
+                elif nums[mid]>k:
+                    j=mid-1
+                else:
+                    i=mid+1
+            return -1
+        def lastele(nums,k):
+            i,j=0,len(nums)-1
+            while i<=j:
+                mid=(i+j)//2
+                if nums[mid]==k:
+                    if mid==len(nums)-1 or nums[mid+1]!=k:
+                        return mid
+                    else:
+                        i=mid+1
+                elif nums[mid]>k:
+                    j=mid-1
+                else:
+                    i=mid+1
+            return -1
+        return firstele(nums,target),lastele(nums,target)
