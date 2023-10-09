4f8451c9d84b5516400d02f89f216a4d565f06d3
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-10-09 10:28:20 +0530

    Time: 90 ms (20.85%), Space: 17.7 MB (50.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -14,18 +14,7 @@ class Solution:
                 else:
                     i=mid+1
             return -1
-        def lastele(nums,k):
-            i,j=0,len(nums)-1
-            while i<=j:
-                mid=(i+j)//2
-                if nums[mid]==k:
-                    if mid==len(nums)-1 or nums[mid+1]!=k:
-                        return mid
-                    else:
-                        i=mid+1
-                elif nums[mid]>k:
-                    j=mid-1
-                else:
-                    i=mid+1
-            return -1
-        return firstele(nums,target),lastele(nums,target)
+        m=firstele(nums,target)
+        while m<len(nums)-1 and nums[m+1]==target:
+            m+=1
+        return firstele(nums,target),m
