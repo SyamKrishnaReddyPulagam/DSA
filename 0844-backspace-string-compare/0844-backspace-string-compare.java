2311ee071305f102712e7cbaaa6493486baa86c2
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-10-19 11:38:32 +0530

    Time: 8 ms (5.74%), Space: 41.8 MB (6.26%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,36 +1,27 @@
 class Solution {
-    public static boolean backspace(String s, String t) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '#' && !stack.isEmpty()) {
-                stack.pop();
-            } else if (Character.isLetter(c)) {
-                stack.push(c);
-            }
-        }
-
-        Stack<Character> stack1 = new Stack<>();
-        for (char c : t.toCharArray()) {
-            if (c == '#' && !stack1.isEmpty()) {
-                stack1.pop();
-            } else if (Character.isLetter(c)) {
-                stack1.push(c);
-            }
-        }
-
-        StringBuilder sbStack = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sbStack.insert(0, stack.pop());
-        }
-
-        StringBuilder sbStack1 = new StringBuilder();
-        while (!stack1.isEmpty()) {
-            sbStack1.insert(0, stack1.pop());
-        }
-
-        return sbStack.toString().equals(sbStack1.toString());
-    }
     public boolean backspaceCompare(String s, String t) {
-        return backspace(s, t);
+        String s1 = "";
+        for (char i : s.toCharArray()) {
+            if (Character.isLetter(i)) {
+                s1 += i;
+            } else {
+                if (!s1.isEmpty()) {
+                    s1 = s1.substring(0, s1.length() - 1);
+                }
+            }
+        }
+
+        String t1 = "";
+        for (char i : t.toCharArray()) {
+            if (Character.isLetter(i)) {
+                t1 += i;
+            } else {
+                if (!t1.isEmpty()) {
+                    t1 = t1.substring(0, t1.length() - 1);
+                }
+            }
+        }
+
+        return s1.equals(t1);
     }
 }
