66655653616dcfa7317f4d7c4d1045388a16544f
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-11-04 18:23:09 +0530

    Time: 143 ms (82.01%), Space: 17.3 MB (67.63%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.py
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -1,7 +1,13 @@
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
-        if not right:
-            return max(left)
-        if not left:
-            return n-min(right)
-        return max(n-min(right),max(left))
+        if right and left:
+            a1=max(left)
+            a2=min(right)
+            return max(n-a2,a1)
+        elif not right:
+            a1=max(left)
+            return a1
+        else:
+            a2=min(right)
+            return n-a2
+        
