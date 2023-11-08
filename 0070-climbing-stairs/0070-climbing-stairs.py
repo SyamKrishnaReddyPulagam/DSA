9f278fc4b9db37b6b85e3ab2b92d8a476065a5e8
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-11-08 11:45:44 +0530

    Time: 32 ms (88.67%), Space: 16.2 MB (35.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,10 @@
-class Solution(object):
-    def climbStairs(self, n):
-        a, b = 1, 1
-        for i in range(n):
-            a, b = b, a + b
-        return a
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<4:
+            return n
+        arr=[0]*(n+1)
+        arr[-1]=1
+        arr[-2]=1
+        for i in range(n-2,-1,-1):
+            arr[i]=arr[i+1]+arr[i+2]
+        return arr[0]
