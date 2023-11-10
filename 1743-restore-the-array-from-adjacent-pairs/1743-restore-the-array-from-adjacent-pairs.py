69d444c8f0d40c78faa43967b5a2ddfbd4afd8f6
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-11-10 15:11:11 +0530

    Time: 921 ms (57.66%), Space: 64.9 MB (87.10%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
-        n=len(adjacentPairs)
+        """n=len(adjacentPairs)
         if n==1:
             return adjacentPairs[0]
         arrlen=(n*2)-2
@@ -34,3 +34,20 @@ class Solution:
                 if j not in nset:
                     start=j
         return nums
+        """
+        
+        adjacent = defaultdict(list)
+        for u, v in adjacentPairs:
+            adjacent[u].append(v)
+            adjacent[v].append(u)
+
+        array = []
+        for num, neighbours in adjacent.items():
+            if len(neighbours) == 1:
+                array = [num, neighbours[0]]
+                break
+
+        while len(array) < len(adjacentPairs) + 1:
+            u, v = adjacent[array[-1]]
+            array.append(u if array[-2] == v else v)
+        return array
