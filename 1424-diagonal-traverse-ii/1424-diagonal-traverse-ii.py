40b957e2006daa62516af0e948eff264426ced51
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-11-22 19:25:29 +0530

    Time: 797 ms (48.62%), Space: 39.8 MB (63.54%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        ans=[]
+        dicti=defaultdict(list)
         n=len(nums)
         if n==1:
-            return [x for x in nums[0]]
-        for i in range(n-1,-1,-1):
-            z=nums[i]
-            for j in range(len(z)):
-                ans.append((i+j,-i,z[j]))
-        heapq.heapify(ans)
-        x=len(ans)
-        y=heapq.nsmallest(x,ans)
-        return [i[2] for i in y ]
+            return nums[0]
+        for i in range(n):
+            rowi=nums[i]
+            for j in range(len(rowi)):
+                dicti[i+j]=[nums[i][j]]+dicti[i+j]
+        final=[]
+        z=dicti.values()
+        for i in z:
+            final+=i
+        return final
