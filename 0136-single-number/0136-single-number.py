c305cdf752d5ac1ac9d9032ce23bbb46f3c03f73
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-11-23 20:21:00 +0530

    Time: 119 ms (81.27%), Space: 19 MB (53.07%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def singleNumber(self, nums):
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
         n=0
         for i in nums:
             n^=i
