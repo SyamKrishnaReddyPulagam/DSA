7bf699202bffea260968cf1b222f43cc146567b6
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-11-23 20:04:49 +0530

    Time: 35 ms (85.78%), Space: 16.3 MB (29.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,11 +1,7 @@
 class Solution:
-    def reverseBits(self, n):
-        n=bin(n).replace("0b","")
-        n=str(n)
-        x=32-len(n)
-        y=""
-        for i in range(x):
-            y+='0'
-        n=y+n
-        n=n[::-1]
-        return int(n,2)
+    def reverseBits(self, n: int) -> int:
+        ans=0
+        for i in range(32):
+            ans=(ans << 1) | (n & 1)
+            n>>=1
+        return ans
