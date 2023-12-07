9cae1ae6c6c9ef479ea9ed2b071672babba6ce96
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-12-07 18:19:16 +0530

    Time: 14 ms (76.69%), Space: 13.2 MB (26.32%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def totalMoney(self, n):
         weeks,days=n//7,n%7
-        monday=1
+        monday=weeks+1
         ans=0
-        while weeks>=1:
-            ans+=(7*(2*monday+6))/2
-            monday+=1
-            weeks-=1
+        if weeks>0:
+            ans+=((weeks)*(2*28+(weeks-1)*7))/2
         if days>=1:
             ans+=(days*(2*monday+(days-1)))/2
         return ans
