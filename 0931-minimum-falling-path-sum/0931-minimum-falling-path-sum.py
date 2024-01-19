a23af229eb989cecdaf53cc7e46a12f1e4c205bf
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2024-01-19 09:11:49 +0530

    Time: 101 ms (63.13%), Space: 14.3 MB (13.41%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,17 +1,17 @@
-class Solution:
-    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+class Solution(object):
+    def minFallingPathSum(self, matrix):
         n=len(matrix)
         temp,ans=0,float("inf")
-        for row in range(n-2,-1,-1):
+        for row in range(1,n):
             for col in range(n):
                 if col>0:
-                    left=matrix[row+1][col-1]
+                    left=matrix[row-1][col-1]
                 else:
                     left=float("inf")
-                mid=matrix[row+1][col]
+                mid=matrix[row-1][col]
                 if col==n-1:
                     right=float("inf")
                 else:
-                    right=matrix[row+1][col+1]
+                    right=matrix[row-1][col+1]
                 matrix[row][col]=matrix[row][col]+min(left,mid,right)
-        return min(matrix[0])
+        return min(matrix[-1])
