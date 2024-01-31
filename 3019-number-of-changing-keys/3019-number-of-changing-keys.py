f82f862055300f36aa0f557de18ccbae94f70ab8
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2024-01-31 21:51:40 +0530

    Time: 41 ms (25.94%), Space: 16.7 MB (8.81%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.py
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.py
@@ -1,6 +1,5 @@
 class Solution:
     def countKeyChanges(self, s: str) -> int:
-        dicti={'A':'a','B':'b','C':'c','D':'d','E':'e','F':'f','G':'g','H':'h','I':'i','J':'j','K':'k','L':'l','M':'m','N':'n','O':'o','P':'p','Q':'q','R':'r','S':'s','T':'t','U':'u','V':'v','W':'w','X':'x','Y':'y','Z':'z'}
         n=len(s)
         i=1
         ans=0
