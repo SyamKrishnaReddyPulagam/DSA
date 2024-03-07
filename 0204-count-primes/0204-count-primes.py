d6c0bf5e06a61993d553fee8dfca47e0d6afbecf
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2024-03-07 21:45:48 +0530

    Time: 2786 ms (23.12%), Space: 74 MB (38.59%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,10 +3,9 @@ class Solution:
         if n<2:
             return 0
         prime=[True for i in range(n)]
-        i=2
-        while i*i<n:
-            if prime[i]:
-                for j in range(i*i,n,i):
+        for i in range(2,n):
+            c=prime[i]
+            if c:
+                for j in range(i+i,n,i):
                     prime[j]=False
-            i+=1
         return prime.count(True)-2
