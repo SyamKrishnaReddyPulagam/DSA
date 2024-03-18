ef17ae77712ce6a57784b2c5eaac05dcb378c586
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2024-03-18 21:49:32 +0530

    Time: 647 ms (19.43%), Space: 16.7 MB (30.15%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,14 +1,14 @@
-class Solution:
-    def findMaxLength(self, nums: List[int]) -> int:
-        ones,dicti=0,{}
-        ans=0
+class Solution(object):
+    def findMaxLength(self, nums):
+        dicti={0:-1}
+        prefix,ans=0,0
         for i in range(len(nums)):
-            if nums[i]==1:
-                ones+=1
-            if ones-(i+1-ones) not in dicti:
-                dicti[ones-(i+1-ones)]=i
-            if ones==i+1-ones:
-                ans=i+1
+            if nums[i]==0:
+                prefix+=-1
             else:
-                ans=max(ans,i-dicti[ones-(i+1-ones)])
+                prefix+=nums[i]
+            if prefix not in dicti:
+                dicti[prefix]=i
+            else:
+                ans=max(ans,i-dicti[prefix])
         return ans
