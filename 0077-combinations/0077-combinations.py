a2ab36fe4d77f12ad6659a9e0c2e9f89da99d873
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2024-03-23 17:02:33 +0530

    Time: 706 ms (71.17%), Space: 64.8 MB (29.40%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        arr=[i+1 for i in range(n)]
-        subsets=[[arr[0]]]
-        for i in range(1,len(arr)):
-            ele=arr[i]
-            ap=[[ele]]
-            for j in subsets:
-                ap.append(j+[ele])
-            subsets+=ap
         ans=[]
-        for i in subsets:
-            if len(i)==k:
-                ans.append(i)
+        def func(nums,arr,i,k):
+            if len(arr)==k:
+                nonlocal ans
+                ans.append(arr.copy())
+                return
+            if i==len(nums):
+                return
+            arr.append(nums[i])
+            func(nums,arr,i+1,k)
+            arr.pop()
+            func(nums,arr,i+1,k)
+        nums=[i for i in range(1,n+1)]
+        func(nums,[],0,k)
         return ans
