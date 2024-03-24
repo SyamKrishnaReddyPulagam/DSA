8edfc3a1d8ac8d41535df48a59f0eeec5c4c5eb6
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2024-03-24 16:05:55 +0530

    Time: 78 ms (14.45%), Space: 11.6 MB (85.55%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,25 +1,39 @@
-class Solution:
-    def totalNQueens(self, n: int) -> int:
-        col,posD,negD=set(),set(),set()
-        ans=0
-        board=[["."]*n for i in range(n)]
-        def func(row):
-            if row==n:
-                nonlocal ans
-                ans+=1
+class Solution(object):
+    def totalNQueens(self, n):
+        ans=[0]
+        board=[["." for _ in range(n)] for _ in range(n)]
+        def issafe(row,col,board,n):
+            r,c=row,col
+            #left upper diagnol
+            while r>=0 and c>=0:
+                if board[r][c]=="Q":
+                    return False
+                r-=1
+                c-=1
+            
+            r,c=row,col
+            #right lower diagnol
+            while r<n and c>=0:
+                if board[r][c]=="Q":
+                    return False
+                r+=1
+                c-=1
+            
+            r,c=row,col
+            #row 
+            while c>=0:
+                if board[r][c]=="Q":
+                    return False
+                c-=1
+            return True
+        def func(col,board,n,ans):
+            if col==n:
+                ans[0]+=1
                 return
-            for c in range(n):
-                if c in col or (row+c) in posD or (row-c) in negD:
-                    continue
-                col.add(c)
-                posD.add(row+c)
-                negD.add(row-c)
-                
-                func(row+1)
-                
-                col.remove(c)
-                posD.remove(row+c)
-                negD.remove(row-c)
-        func(0)
-        return ans
-                
+            for r in range(n):
+                if issafe(r,col,board,n):
+                    board[r][col]="Q"
+                    func(col+1,board,n,ans)
+                    board[r][col]="."
+        func(0,board,n,ans)
+        return ans[0]
