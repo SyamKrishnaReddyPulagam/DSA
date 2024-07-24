a408f8c46290e77a597ecdb065e7f237a297270d
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2024-07-24 22:41:24 +0530

    Time: 762 ms (98.37%), Space: 24.2 MB (70.11%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -4,22 +4,12 @@ class Solution:
         dicti={}
         for i in range(len(mapping)):
             dicti[str(i)]=str(mapping[i])
-        arr=[]
+        arr={}
         for i in range(len(nums)):
             cur=str(nums[i])
             new=""
             for j in cur:
                 new+=dicti[j]
-            arr.append(int(new))
-        temp=SortedDict()
-        for i in range(len(arr)):
-            if arr[i] in temp:
-                temp[arr[i]].append(nums[i])
-            else:
-                temp[arr[i]]=[nums[i]]
-        i=0
-        for j in temp:
-            for k in temp[j]:
-                nums[i]=k
-                i+=1
+            arr[nums[i]]=int(new)
+        nums.sort(key=lambda x:arr[x])
         return nums
