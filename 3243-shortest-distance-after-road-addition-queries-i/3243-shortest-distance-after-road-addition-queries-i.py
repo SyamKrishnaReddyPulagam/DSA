2951025da783b64fcee74b5bf464e2bf4d3943e7
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2024-08-06 21:49:11 +0530

    Time: 1868 ms (18.07%), Space: 16.9 MB (81.97%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -4,13 +4,12 @@ class Solution:
         for i in range(n):
             dicti[i]=float("inf")
         dicti[0]=0
-        edges={i:[i+1] for i in range(n-1)}
+        edges={i:[i+1] for i in range(n)}
+        edges[n-1]=[]
         def func(edges,dicti):
-            cur_node=0
-            while cur_node!=n-1:
-                for i in edges[cur_node]:
-                    dicti[i]=min(dicti[i],dicti[cur_node]+1)
-                cur_node+=1
+            for cur_node in range(n-1):
+                for node in edges[cur_node]:
+                    dicti[node]=min(dicti[node],dicti[cur_node]+1)
             return dicti
         ans=[]
         for i,j in queries:
